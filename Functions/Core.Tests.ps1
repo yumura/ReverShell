6468519ef20bd5f528ce3433e409dbe6f120c798
yumura_s <nakamura.shunki.1990+yumura@gmail.com>
2016-05-11 07:00:16 +0900

    [add] 逆ポーランド記法を解釈する関数を追加
    テストコードのように fun と word で関数を定義できる。

---
+++ Functions/Core.Tests.ps1
@@ -0,0 +1,78 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".Tests.", ".")
+. "$here\$sut"
+
+Describe "Core" {
+    It "四則演算が定義できる" {
+        fun + "x y -- z" {$_0 + $_1} 2
+        fun - "x y -- z" {$_0 - $_1} 2
+        fun * "x y -- z" {$_0 * $_1} 2
+        fun / "x y -- z" {$_0 / $_1} 2
+
+        RPN 3 4 2 * 1 5 - / + | Pop-RPN | Should Be 1
+    }
+
+    It "単項演算子が定義できる" {
+        fun negate 'x -- y' {- $_0} 1
+        RPN   3  negate | Pop-RPN | Should Be (-3)
+        RPN (-3) negate | Pop-RPN | Should Be   3
+        RPN   3  negate negate | Pop-RPN | Should Be 3
+    }
+
+    It "n項演算子が定義できる" {
+        $sum = {$args | %{$w=0}{$w+=$_}{$w}}
+
+        foreach ($i in 1..4)
+        {
+            $name   = "sum${i}"
+            $xs     = (1..$i | %{"x${_}"}) -join ' ' 
+            $effect = "${xs} -- y"
+
+            fun $name $effect $sum $i
+        }
+
+        RPN 1       sum1 | Pop-RPN | Should Be  1
+        RPN 1 2     sum2 | Pop-RPN | Should Be  3
+        RPN 1 2 3   sum3 | Pop-RPN | Should Be  6
+        RPN 1 2 3 4 sum4 | Pop-RPN | Should Be 10    
+    }
+
+    It "先頭がコロンであれば文字列として扱われる" {
+        fun + "x y -- z" {$_0 + $_1} 2
+
+        RPN :Hello :_  :World + + | Pop-RPN | Should Be 'Hello_World'
+    }
+
+    It "逆ポーランド記法で関数を定義できる" {
+        fun dup  'x -- x x'   {$_0, $_0} 1
+        fun swap 'x y -- y x' {$_1, $_0} 2
+
+        word sq  'x -- y' dup *
+        word neg 'x -- y' 0 swap -
+
+        RPN 5 sq  | Pop-RPN | Should Be  25
+        RPN 3 neg | Pop-RPN | Should Be (-3)
+    }
+
+    It "クォートを作成できる (q ...)" {
+        fun if '? true false -- node' {
+            param($stack)
+
+            if ($_0) {,$stack | RPN @_1}
+            else     {,$stack | RPN @_2}
+
+        } 3 -IsWord
+
+        fun drop 'x -- ' {} 1
+        fun eq 'x y -- ?' {$_0 -eq $_1} 2
+        fun lt 'x y -- ?' {$_0 -lt $_1} 2
+        fun gt 'x y -- ?' {$_0 -gt $_1} 2
+
+        word zero? 'x -- ?' 0 eq
+        word sign-test 'x -- str' dup 0 lt (q drop :negative) (q zero? (q :zero) (q :positive) if) if
+
+        RPN   1  sign-test | Pop-RPN | Should Be 'positive'
+        RPN   0  sign-test | Pop-RPN | Should Be 'zero'
+        RPN (-1) sign-test | Pop-RPN | Should Be 'negative'
+    }
+}
---
+++ Functions/Core.ps1
@@ -0,0 +1,120 @@
+﻿# Variable
+# ========
+$Script:RPNDictionary = @{}
+$Script:StringHead = ':'
+
+
+# Function
+# ========
+filter Global:Invoke-ReversePolishNotation
+{
+    $stack = if ($MyInvocation.ExpectingInput) {,$_}
+             else {New-Object System.Collections.Stack}
+
+    $args | %{
+        if (Test-RPNValue $_) {Push-RPN $_ $stack}
+        else {$stack = RPNEval $_ $stack}
+    }
+
+    ,$stack
+}
+
+filter Test-RPNValue ($Token)
+    {$Token -isnot [string] -or $Token.StartsWith($Script:StringHead)}
+
+filter Push-RPN ($Token, $Stack)
+{
+    if ($Token -isnot [string]) {return $stack.Push($Token)}
+
+    $tail = $Token -replace "^${Script:StringHead}", ''
+    $stack.Push($tail)
+}
+
+filter RPNEval ($name, $stack)
+{
+    $dict = $Script:RPNDictionary
+    if (-not $dict.ContainsKey($name))
+        {throw "関数 ${name} が定義されていません"}
+
+    $fun = $dict[$name]
+    if ($stack.Count -lt $fun.ArgsLength)
+        {throw "関数 ${name} に対する被演算子が足りません"}
+
+    if ($fun.IsWord) {return RPNEvalWord $fun $stack}
+    RPNEvalFunction $fun $stack
+}
+
+filter RPNEvalWord ($fun, $stack)
+{
+    if ($fun.ArgsLength -le 0)
+        {return ,(& $fun.ScriptBlock -stack $stack)}
+        
+    $argstack = New-Object System.Collections.Stack
+    foreach ($i in ($fun.ArgsLength - 1)..0)
+    {
+        $value = $stack.Pop()
+        Set-Variable "_${i}" $value
+        $argstack.Push($value)
+    }
+    ,(& $fun.ScriptBlock -stack $stack)
+}
+
+filter RPNEvalFunction ($fun, $stack)
+{
+    if ($fun.ArgsLength -le 0)
+    {
+        & $fun.ScriptBlock | %{$stack.Push($_)}
+        return ,$stack
+    }    
+    
+    $argstack = New-Object System.Collections.Stack
+    foreach ($i in ($fun.ArgsLength - 1)..0)
+    {
+        $value = $stack.Pop()
+        Set-Variable "_${i}" $value
+        $argstack.Push($value)
+    }
+    & $fun.ScriptBlock @argstack | %{$stack.Push($_)}
+    return ,$stack
+}
+
+filter New-RPNFunction
+{
+    Param
+    (
+        [string]      $Name,
+        [string]      $Effect,
+        [ScriptBlock] $ScriptBlock,
+        [int]         $ArgsLength,
+        [switch]      $IsWord
+    )
+    
+    if (-not $IsWord) {$PSBoundParameters['IsWord'] = $false}
+    $Script:RPNDictionary[$Name] = New-Object PSCustomObject -Property $PSBoundParameters
+}
+
+filter New-RPNWord
+{
+    $name, $effect, $tail = $args
+    $word = {
+        param($stack)
+        ,$stack | Global:Invoke-ReversePolishNotation @tail
+    }.GetNewClosure()
+
+    New-RPNFunction $name $effect $word 0 -IsWord
+}
+
+filter Get-RPNFunction ([string] $Name) {$Script:RPNDictionary[$Name]}
+
+filter New-RPNQuote {,$args}
+
+filter Pop-RPN {$_.Pop()}
+
+
+# Alias
+# =====
+Set-Alias RPN  Invoke-ReversePolishNotation
+Set-Alias fun  New-RPNFunction
+Set-Alias word New-RPNWord
+Set-Alias q    New-RPNQuote
+

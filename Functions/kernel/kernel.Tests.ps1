29d499a734bd0efc41a846dcb628b28fdc929b88
yumura_s <nakamura.shunki.1990+yumura@gmail.com>
2016-05-13 19:27:17 +0900

    [add] new vocabulary file

---
+++ Functions/kernel/kernel.Tests.ps1
@@ -0,0 +1,18 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".Tests.", ".")
+
+$parent = Split-Path -Parent $here
+
+. "$parent\Core.ps1"
+. "$here\$sut"
+
+filter ShouldBeRPN
+{
+    $stack1 = $_
+    $stack2 = RPN @args
+
+    while ($stack1.Count -gt 0)
+        {$stack1.Pop() | Should Be $stack2.Pop()}
+            
+    $stack1.Count | Should Be $stack2.Count
+}

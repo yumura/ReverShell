bbc6c4363a5819ba4366009041c5158e1dbcdc63
yumura_s <nakamura.shunki.1990+yumura@gmail.com>
2016-05-13 19:45:01 +0900

    [add]  new test - Stack stuff

--- Functions/kernel/kernel.Tests.ps1
+++ Functions/kernel/kernel.Tests.ps1
@@ -16,3 +16,39 @@ filter ShouldBeRPN
             
     $stack1.Count | Should Be $stack2.Count
 }
+
+Describe "Stack stuff" {
+    It "dropers" {
+        RPN 0 1        drop | ShouldBeRPN 0
+        RPN 0 1 2     2drop | ShouldBeRPN 0
+        RPN 0 1 2 3   3drop | ShouldBeRPN 0
+        RPN 0 1 2 3 4 4drop | ShouldBeRPN 0
+    }
+
+    It "dupers" {
+        RPN 0        dup | ShouldBeRPN 0 0
+        RPN 0 1     2dup | ShouldBeRPN 0 1 0 1
+        RPN 0 1 2   3dup | ShouldBeRPN 0 1 2 0 1 2
+        RPN 0 1 2 3 4dup | ShouldBeRPN 0 1 2 3 0 1 2 3
+    }
+
+    It "nipers" {
+        RPN 0 1        nip | ShouldBeRPN 1
+        RPN 0 1 2     2nip | ShouldBeRPN 2
+        RPN 0 1 2 3   3nip | ShouldBeRPN 3
+        RPN 0 1 2 3 4 4nip | ShouldBeRPN 4
+    }
+
+    It "shuffle!" {
+        RPN 0 1 2 rot-  | ShouldBeRPN 2 0 1
+        RPN 0 1   dupd  | ShouldBeRPN 0 0 1
+        RPN 0 1   over  | ShouldBeRPN 0 1 0
+        RPN 0 1 2 pick  | ShouldBeRPN 0 1 2 0 
+        RPN 0 1 2 rot   | ShouldBeRPN 1 2 0
+        RPN 0 1   swap  | ShouldBeRPN 1 0
+        RPN 0 1 2 swapd | ShouldBeRPN 1 0 2
+
+        RPN 0 1 2 2over | ShouldBeRPN 0 1 2 0 1
+        RPN 0 1 2 clear | ShouldBeRPN 
+    }
+}
